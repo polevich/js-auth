58a6a30e2ded64dbbcd774ed239cbfef34658366
Yurii Polevich <polevichx@gmail.com>
2024-06-28 21:51:56 +0300

    first commit

---
+++ src/route/auth.js
@@ -0,0 +1,46 @@
+// Підключаємо технологію express для back-end сервера
+const express = require('express')
+// Cтворюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
+
+const { User } = require('../class/user')
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/signup', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  return res.render('signup', {
+    // вказуємо назву контейнера
+    name: 'signup',
+    // вказуємо назву компонентів
+    component: ['back-button', 'field'],
+
+    // вказуємо назву сторінки
+    title: 'Signup page',
+    // ... сюди можна далі продовжувати додавати потрібні технічні дані, які будуть використовуватися в layout
+    // вказуємо дані,
+    data: {
+      role: [
+				{value: User.USER_ROLE.USER, text: "Пользователь"},
+				{
+					value: User.USER_ROLE.ADMIN,
+					text: "Администратор",
+				},
+				{
+					value: User.USER_ROLE.DEVELOPER,
+					text: "Разработчик",
+				},
+			]
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+
+// Підключаємо роутер до бек-енду
+module.exports = router
