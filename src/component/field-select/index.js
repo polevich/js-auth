216be0d9eec828c3456db1f3fd0e09345255f89c
Yurii Polevich <polevichx@gmail.com>
2024-07-01 18:38:31 +0300

    4 commit

--- src/component/field-select/index.js
+++ src/component/field-select/index.js
@@ -7,6 +7,7 @@ class FieldSelect {
 			window.addEventListener(
 				'click',
 				(e) => {
+					alert(123)
 					if (!options.parentElement.contains(e.target))
 						options.removeAttribute('active')
 				},
